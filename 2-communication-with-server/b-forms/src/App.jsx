76849e0f2973105a6f4ad2bfba05bc07853dc165
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-20 22:17:06 +0500

    input and event handler for input tag created with simple function

--- 2-communication-with-server/b-forms/src/App.jsx
+++ 2-communication-with-server/b-forms/src/App.jsx
@@ -3,12 +3,23 @@ import Notes from "./components/Notes"
 
 function App({ notes }) {
   const [note, setNote] = useState(notes)
+
+  const formHandler = (event) => {
+    event.preventDefault()
+    console.log("form data ", event.target);
+
+
+  }
   return (
     <>
       <h1>Notes</h1>
       <ul>
         {note.map(notelist => <Notes key={notelist.id} note={notelist} />)}
       </ul>
+      <form action="" onSubmit={formHandler}>
+        <input />
+        <button type="submite">Submit</button>
+      </form>
     </>
   )
 }
