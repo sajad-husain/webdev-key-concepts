afc2578a627d9c54b3e329dcc796b24ee47fcb2e
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-08-30 08:51:12 +0500

    export syntax changed because key and values are same

--- 2-communication-with-server/d-altering-data-in-server/src/components/services/notes.js
+++ 2-communication-with-server/d-altering-data-in-server/src/components/services/notes.js
@@ -17,8 +17,4 @@ const update = (id, newObj) => {
     return axios.put(`${url}/${id}`, newObj)
 }
 
-export default {
-    getAll: getAll,
-    create: create,
-    update: update
-}
+export default { getAll, create, update }
