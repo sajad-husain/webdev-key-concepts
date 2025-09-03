9e47c063a1b370552237c6b92e14e25b5953e89d
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-03 08:50:58 +0500

    footer ocmponent updated in notes app with desired data

--- 2-communication-with-server/d-altering-data-in-server/src/components/Footer.jsx
+++ 2-communication-with-server/d-altering-data-in-server/src/components/Footer.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
 const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italic'
+    }
+
     return (
-        <div>Footer</div>
+        <div style={footerStyle}>
+            <br />
+            <p>
+                Note app, Department of Computer Science, University of Helsinki 2025
+            </p>
+        </div>
     )
 }
 
