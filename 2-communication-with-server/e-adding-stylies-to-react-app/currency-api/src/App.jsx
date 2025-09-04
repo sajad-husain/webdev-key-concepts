840d8dc3f6fbf54e225fd6ebe6649c3a9d884372
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-04 22:50:59 +0500

    states added to app to handle input, currency , and exchange rates

--- 2-communication-with-server/e-adding-stylies-to-react-app/currency-api/src/App.jsx
+++ 2-communication-with-server/e-adding-stylies-to-react-app/currency-api/src/App.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [value, setValue] = useState('')
+  const [rates, setRates] = useState({})
+  const [currency, setCurrency] = useState(null)
+
+
   return (
     <div>
       <form onSubmit={currencyHandler}>
