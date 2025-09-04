fa1d07187122a42fb3543afdb0ec911ef1af0b00
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-04 22:53:59 +0500

    value can be chnaged using the output

--- 2-communication-with-server/e-adding-stylies-to-react-app/currency-api/src/App.jsx
+++ 2-communication-with-server/e-adding-stylies-to-react-app/currency-api/src/App.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [value, setValue] = useState('')
   const [rates, setRates] = useState({})
   const [currency, setCurrency] = useState(null)
 
+  useEffect(() => {
+    console.log('effect run, currency is now', currency);
 
+  }, [])
   return (
     <div>
       <form onSubmit={currencyHandler}>
-        Currency: <input type="text" />
+        Currency: <input
+          onChange={e => setValue(e.target.value)}
+          value={value}
+          type="text" />
         <button type='submit'>Exchange Rates</button>
       </form>
       { }
