2149daa2aea352b5e30bb3a31d43beaa045ade1e
Sajjad Hussain <sajjadhusain8084@gmail.com>
2025-09-10 23:23:13 +0500

    essential comments added for rest api which include sending varius requests to server like get, post, put, patch and delete etc

--- 3-Node.js and Express/index.js
+++ 3-Node.js and Express/index.js
@@ -19,6 +19,14 @@ let notes = [
     }
 ]
 
+// URL	verb	functionality
+// notes / 10	GET	fetches a single resource
+// notes	GET	fetches all resources in the collection
+// notes	POST	creates a new resource based on the request data
+// notes / 10	DELETE	removes the identified resource
+// notes / 10	PUT	replaces the entire identified resource with the request data
+// notes / 10	PATCH	replaces a part of the identified resource with the request data
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World From HomPage</h1>')
 })
