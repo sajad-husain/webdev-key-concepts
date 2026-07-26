1d4c80168a8aa0d3c44d33bf01426dfe50d8a1a7
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-07-26 21:42:48 +0500

    functions usage

---
+++ JS/functions/script.js
@@ -0,0 +1,11 @@
+const tax1 = 49.99 * 0.1;
+const tax1 = 89.99 * 0.1;
+const tax1 = 21.99 * 0.1;
+
+function calculateTax(value) {
+  return value * 0.1;
+}
+
+const tax1 = calculateTax(34);
+const tax2 = calculateTax(53);
+const tax3 = calculateTax(32);
