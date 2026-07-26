b61eb4b3c4ca8a7bf9628f7050e2638ffbb465c2
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-07-26 21:50:24 +0500

    learned about types of functiona and about pararmeters and arguments

--- JS/functions/script.js
+++ JS/functions/script.js
@@ -9,3 +9,28 @@ function calculateTax(value) {
 const tax1 = calculateTax(34);
 const tax2 = calculateTax(53);
 const tax3 = calculateTax(32);
+
+// funciton declaration
+function greet(name) {
+  return `hello ${name}`;
+}
+
+// functoin expression
+const greet = function (name) {
+  return `helo ${name}`;
+};
+
+// arrow function
+const greet = (name) => {
+  return `helo ${name}`;
+};
+
+// parameters and arguments
+// num1 and num2 are parameter
+// passing value to the funciotons while defining them
+function learningParams(num1, num2) {
+  return num1 + num2;
+}
+
+//passing value to the funciton when calling is arguments
+console.log(learningParams(5, 6));
