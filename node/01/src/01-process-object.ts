895e3c7d2594ade450fa53309d7528ce03a9850a
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-07-31 23:28:08 +0500

    learned about process object in node js and it's applicaiton while applying failed and crash flags in node js appication

--- node/01/src/01-process-object.ts
+++ node/01/src/01-process-object.ts
@@ -9,9 +9,47 @@
 // read CLI arguments in scripts 
 
 import process from "node:process"
+import crypto from "crypto"
 
 // dot env
 
 const nodeEnv = process.env.NODE_ENV ?? "develpment";
 
 // process.env vlaues always should be string or undefined
+const port = Number(process.env.NODE_ENV ?? 3000)
+
+// process.argv[0] path to node
+// process.argv[1] path to url-file in my case it's src/01-process-object.ts
+// process.argv[2] start => we'll start our value from 2nd argv
+
+const commnadArg = process.argv[2] ?? "start";
+
+// crash flag 
+// fail flag
+const shouldFail = process.argv.includes("--fail")
+const shouldCrash = process.argv.includes("--crash")
+
+// dont start async here 
+// node is already shutting down
+// final log, final cleanup
+
+process.on("exit", (code) =>{
+    console.log(`process is finished with exite code ${code}`);
+    
+})
+
+function runApp(): void {
+    console.log({commnadArg,});
+    
+    if(shouldCrash){
+        console.error("Manual crash triggered with  --crash flag")
+        process.exit(1)
+    }
+    
+    if(shouldFail){
+        console.error("Manual fail triggered with  --fail flag")
+        process.exit(1)
+    }
+}
+
+runApp()
