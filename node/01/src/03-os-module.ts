33f33479d7ca5927ae720e344dd0bac158ed3fde
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-08-02 00:05:03 +0500

    learned os modules

---
+++ node/01/src/03-os-module.ts
@@ -0,0 +1,27 @@
+import * as os from "node:os"
+
+// os
+// cpu info, memory, home/temp dir
+
+function runOsDemo(): void {
+    console.log("Plateform", os.platform());
+    console.log("Architecture", os.arch());
+    console.log("OS type", os.type());
+    console.log("OS release", os.release());
+    console.log("Home directory", os.homedir());
+    console.log("Temp directory", os.tmpdir());
+    
+    const cpus = os.cpus()
+    console.log("Cpu's Details", cpus.length)
+
+    if(cpus.length > 0){
+        console.log("FIRST CPU MODEL IS:", cpus[0].model,"SPEED IS" ,cpus[0].speed,"TIMES IS" ,cpus[0].times)
+    }
+
+    const toGB = (bytes:number) => (bytes / (1024 ** 3)).toFixed(2);
+    console.log("free memory", toGB(os.freemem()))
+    console.log("total memory", toGB(os.totalmem()));
+    
+}
+
+runOsDemo()
