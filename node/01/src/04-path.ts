3efe3de119e3c5fb7f69147c9152cbf9ec947968
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-08-02 15:03:38 +0500

    path modules key functions

--- node/01/src/04-path.ts
+++ node/01/src/04-path.ts
@@ -1,2 +1,28 @@
 import path from "node:path"
+import process from "node:process"
+
+
+
+
+
+// uploads/users/43/profile.photo.png
+
+const userUploads = process.cwd()
+const userId = "43"
+const profilePhoto = "profile.photo.png"
+
+const originalFilePath = path.join(userUploads, "users", userId, profilePhoto)
+console.log("Original Path",originalFilePath)
+console.log("Root",userUploads)
+
+const extName = path.extname(originalFilePath)
+console.log("Extension",extName);
+
+const rootFolder = path.dirname(originalFilePath)
+console.log("Folder",rootFolder);
+
+const childFile = path.basename(originalFilePath)
+console.log("File",childFile);
+
+
 
